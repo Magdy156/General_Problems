527a5a0a37632796bebca4c594afa95c09fd16d3
Magdy156 <magdyhamdy156@gmail.com>
2025-02-10 22:46:06 +0200

    coins_comb1

---
+++ DP/coins_comb1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <climits>
+#include <vector>
+
+int main(){
+    int n, sum; cin >> n >> sum;
+    int s[n];
+    for(int i = 0; i < n; i++) cin >> s[i];
+    vector<int> dp(sum + 1, 0);
+    dp[0] = 1;
+    for(int i = 1; i<=sum; i++){
+        for(int it:s){
+            if(i - it >= 0) dp[i] += dp[i-it];
+            if(dp[i] >= 1e9+7) dp[i] -= 1e9+7;
+        }
+    }
+    cout << dp[sum];
+    return 0;
+}
